649c689407e727b8c6a7d17a63b775c153b34fe3
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-02-24 19:25:39 -0300

    feat: adicionando conexão com banco de dados

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
 import farmerRoutes from "./routes/farmerRoutes";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
@@ -21,7 +22,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 //routes
-app.use('/api/', farmerRoutes);
+//app.use('/api/', farmerRoutes);
+
+//teste de banco
+const prisma = new PrismaClient();
+
+async function testConnection() {
+  try {
+    await prisma.$connect();
+    console.log("✅ Conectado ao MongoDB Atlas!");
+  } catch (error) {
+    console.error("❌ Erro ao conectar:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+testConnection();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} 🚀`);

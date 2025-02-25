2fe373028488637e0ea9a6365b3a2f5fae3fb9d7
josehenriquearaujodasilva2 <josehenriquearaujodasilva2@gmail.com>
2025-02-25 15:30:00 -0300

    view: add header in about page

--- spa/src/pages/about.jsx
+++ spa/src/pages/about.jsx
@@ -1,12 +1,14 @@
+import { Header } from "../components/header.jsx"
 import {TeamCard} from "../components/team-card.jsx"
 
 export function About() {
   return (
     <div className="min-h-screen bg-slate-300">
-      <main className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-center mb-8 text-green-800">Sobre nosso sistema GreenTech</h1>
+      <Header />
+      <main className="px-8 mx-auto py-8">
 
-        <section className="mb-12">
+        <section className="mb-12 bg-slate-50 w-full rounded">
+          <h1 className="text-4xl font-bold text-center mb-8 text-green-800">Sobre nosso sistema GreenTech</h1>
           <h2 className="text-2xl font-semibold mb-4 text-green-700">Nossa missão</h2>
           <p className="text-lg text-gray-700">
           Dedicamo-nos a capacitar os agricultores, fornecendo um sistema robusto e fácil de usar para gerenciar suas informações. Nosso aplicativo CRUD (Criar, Ler, Atualizar, Excluir) simplifica o gerenciamento de dados, permitindo que os agricultores se concentrem no que fazem de melhor: cultivar a terra e alimentar o mundo.
